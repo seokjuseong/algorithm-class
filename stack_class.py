9f7f267aeb239bd1680265697049e8a94314295c
seokjuseong <seokjuseong0510@gmail.com>
2025-09-09 17:42:05 +0900

    Stack update 2

--- stack_class.py
+++ stack_class.py
@@ -1,1 +1,50 @@
 #define Stack class with push, pop, is_impty, and size methods
+class ArrayStack:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.array = [None]*capacity
+        self.top = self.capacity
+    def is_empty(self):
+        return self.top == -1
+    def is_full(self):
+        return self.top == self.capacity - 1
+    def push(self, item):
+        if not self.is_full():
+            self.top  += 1
+            self.array[self.top] = item
+            print(f"PUSH: {item!r} -> stack is now {self.array[:self.top + 1]}")
+        else:
+            raise OverflowError("Stack Overflow") #pass 
+    def pop(self):
+        if not self.is_empty():
+            item = self.array[self.top]
+            self.array[self.top] = None
+            self.top -= 1
+            print(f"POP: {item!r} -> stack is now {self.array[:self.top + 1]}")
+            return item
+        else:
+            raise IndexError("Stack Underflow!")
+    def peek(self):
+        if not self.is_empty():
+            return self.array[self.top]
+        return None
+    def size(self):
+        return self.top + 1
+    
+#Test the Stack class 
+def reverse_string(statement):
+    print("\n[1] PUSH 단계----------------------------------\n")
+    st = ArrayStack(len(statement))
+    for char in statement:
+        st.push(char)
+    print("POP단계-----------------------------------\n")
+    out = []
+    while not st.is_empty():
+        out.append(st.pop())
+    result = ''.join(out)
+    print(print(f"최종결과: {result}"))
+    return result
+
+if __name__ == "__main__":
+    statements = "안녕하세요 반갑습니다"
+    reverse_string(statements)
